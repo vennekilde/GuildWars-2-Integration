c7fb552c3d36745d774ea7ae3a7ae7d70a45640c
vennekilde <vennekilde@outlook.com>
2016-10-01 01:11:25 +0200

    Added deep merge

--- src/js/common.js
+++ src/js/common.js
@@ -76,11 +76,9 @@ function decodeEntities(encodedString) {
 }
 
 function mergeObjects(obj1,obj2){
-    var obj3 = {};
-    for (var attrname in obj1) { obj3[attrname] = obj1[attrname]; }
-    for (var attrname in obj2) { obj3[attrname] = obj2[attrname]; }
-    return obj3;
+    return jQuery.extend(true, obj1, obj2);
 }
+
 
 function scrollToBottom(container){
     $(container).scrollTop($(container)[0].scrollHeight);

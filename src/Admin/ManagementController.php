2143dced7d6025b136ff6fc0b93479bd18237f97
Jeppe Vennekilde <jeppe@jeppes.info>
2017-12-17 05:51:39 +0100

    Removed debug error reporting that would mess up rest responses

--- src/Admin/ManagementController.php
+++ src/Admin/ManagementController.php
@@ -42,10 +42,6 @@ use function GuzzleHttp\json_encode;
  * THE SOFTWARE.
  */
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 require_once __DIR__ . "/RestrictAdminPanel.php";
 
 $form = filter_input(INPUT_POST, 'form');
